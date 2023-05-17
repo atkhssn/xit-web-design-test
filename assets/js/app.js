5d5420d891a57744c16771e98099f9c5a288171b
atkhssn <atk.hssn2017@gmail.com>
2023-05-17 10:50:42 +0600

    some bug fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,19 @@
+/*
+    Project Name: Multi Purpose Template
+    Design By: atkhssn - MD ATIK HASSAN
+    Version: 1.0.1
+    Copyright: XIT
+*/
+
 $(window).scroll(function() {
 
   // sticky header
   if ($(this).scrollTop() > 115){
-
     $('section.header').addClass("shadow-lg p-3 mb-5 sticky-header fixed-top");
     // console.log('scrolling..');
-
   }
   else{
-
     $('section.header').removeClass("shadow-lg p-3 mb-5 sticky-header fixed-top");
-
   }
 
   // top to bottom
