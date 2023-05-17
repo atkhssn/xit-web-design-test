5e888f5c957b137cfef107de1989e228aa0acf61
atkhssn <atk.hssn2017@gmail.com>
2023-05-17 10:36:18 +0600

    template desogn for xit

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+$(window).scroll(function() {
+
+  // sticky header
+  if ($(this).scrollTop() > 115){
+
+    $('section.header').addClass("shadow-lg p-3 mb-5 sticky-header fixed-top");
+    // console.log('scrolling..');
+
+  }
+  else{
+
+    $('section.header').removeClass("shadow-lg p-3 mb-5 sticky-header fixed-top");
+
+  }
+
+  // top to bottom
+  if ($(this).scrollTop() > 250){  
+    $('div.back-to-top').addClass("backToTop");
+    // console.log('working...');
+  }
+  else{
+    $('div.back-to-top').removeClass("backToTop");
+  }
+
+});
+
+
